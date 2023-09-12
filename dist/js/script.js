c52c7cc68259345201f87deeb4860ba85e58903f
azzamdzikrullah <azzamsayang977@gmail.com>
2023-09-12 12:12:16 +0700

    portfolio tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+// humbuerger
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+});
+
+// navbar fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+
+    if(window.pageYOffset > fixednav) {
+        header.classList.add('navbar-fixed')
+    }else {
+        header.classList.remove('navbar-fixed');
+    }
+}
